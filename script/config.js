f07d90c6bed9b81e69769b63321aa1490e71a1a5
Andrej Gajdos <andrej.gajdos@toptal.com>
2018-06-22 15:00:34 +0200

    changing server url

--- script/config.js
+++ script/config.js
@@ -1,2 +1,2 @@
 export const DATE_FORMAT = 'MMM DD, YYYY';
-export const API_ADDRESS = '//localhost:1337';
+export const API_ADDRESS = (process.env.NODE_ENV && process.env.NODE_ENV === 'production') ? '//newsfeed-cryptogoods.herokuapp.com' : '//localhost:1337';
